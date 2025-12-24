34762b8a17420f5b0d99f3db74ca91b0a1e16594
Artem Suprun <efogdev@gmail.com>
2025-12-24 04:22:27 +0200

    feat: huge power consumption optimization (PM for UART)

--- boards/arm/efogtech_trackball_0/pinmux.c
+++ boards/arm/efogtech_trackball_0/pinmux.c
@@ -4,9 +4,14 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/sys_io.h>
 #include <zephyr/devicetree.h>
+#include <zmk/events/usb_conn_state_changed.h>
+#include <zmk/event_manager.h>
+#include <zephyr/device.h>
+#include <zephyr/pm/device.h>
 
-const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
-const struct device *p1 = DEVICE_DT_GET(DT_NODELABEL(gpio1));
+static const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+static const struct device *p1 = DEVICE_DT_GET(DT_NODELABEL(gpio1));
+static const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
 
 static void set_3v3_en(const bool en) {
     gpio_pin_configure(p1, 0, GPIO_OUTPUT);
@@ -31,3 +36,18 @@ static int pinmux_efgtch_trckbl_init(void) {
 }
 
 SYS_INIT(pinmux_efgtch_trckbl_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+static int usb_conn_chg(const zmk_event_t *eh) {
+    if (zmk_usb_get_conn_state() == ZMK_USB_CONN_HID) {
+        pm_device_action_run(uart, PM_DEVICE_ACTION_RESUME);
+    } else {
+        pm_device_action_run(uart, PM_DEVICE_ACTION_SUSPEND);
+    }
+
+    return 0;
+}
+
+ZMK_SUBSCRIPTION(board_root, zmk_usb_conn_state_changed);
+ZMK_LISTENER(board_root, usb_conn_chg)
+#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
